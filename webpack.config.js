458d60a3c8fa099c72092380f5987f1fef10660f
qtips <ahm.qadeer@gmail.com>
2017-09-01 22:53:56 +0200

    mer notater

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
     devtool: 'source-map',
     entry: [
-        'babel-polyfill', // bundle.js vil inneholde polyfill koden for transformasjon og vår App.js
-        './src/App.js'
+        'babel-polyfill', // bundle.js vil inneholde polyfill koden for transformasjon og vår App.js, uten denne vil ikke async og await
+        './src/App.js' //hoved js fila.
     ],
     output: {                    // webpack lager bundle og putter det her
         filename: 'bundle.js',
@@ -13,16 +13,16 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'www'), //statiske filer lokasjon
         port:1234
-        //quiet:false // debug?
+        //quiet:true // viser mindre output
     },
     module: {
         rules: [
-            { // for alle js filer bruker babel-loader under src mappe
+            { // for alle js filer under src mappen, bruke babel-loader
                 test: /\.js$/,
                 include: path.resolve(__dirname, 'src'), //dirname er node konstant
                 loader: 'babel-loader'
             },
-            {
+            { // for alle css og scsss
                 test: /\.(css|scss)$/,
                 use: [{
                     loader:'style-loader',
